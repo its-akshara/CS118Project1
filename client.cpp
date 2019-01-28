e2ed036947e8388e71f4fcf5ca9dfe6c58f77498
Akshara <akshara.sundar@gmail.com>
2019-01-28 02:09:01 +0000

    Finished diving client into functions

--- client.cpp
+++ client.cpp
@@ -44,6 +44,48 @@ sockaddr_in createClientAddr(int sockfd)
     return clientAddr;
 }
 
+void connectionSetup(struct sockaddr_in clientAddr)
+{
+    char ipstr[INET_ADDRSTRLEN] = {'\0'};
+    inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+    cout << "Set up a connection from: " << ipstr << ":" <<
+    ntohs(clientAddr.sin_port) << endl;
+}
+
+void communicate(int sockfd)
+{
+    // send/receive data to/from connection
+    bool isEnd = false;
+    string input;
+    char buf[20] = {0};
+    stringstream ss;
+    
+    while (!isEnd) {
+        memset(buf, '\0', sizeof(buf));
+        
+        cout << "send: ";
+        cin >> input;
+        if (send(sockfd, input.c_str(), input.size(), 0) == -1) {
+            perror("send");
+            exit(4);
+        }
+        
+        
+        if (recv(sockfd, buf, 20, 0) == -1) {
+            perror("recv");
+            exit(5);
+        }
+        ss << buf << std::endl;
+        cout << "echo: ";
+        cout << buf << std::endl;
+        
+        if (ss.str() == "close\n")
+            break;
+        
+        ss.str("");
+    }
+}
+
 int
 main()
 {
@@ -55,43 +97,10 @@ main()
   serverConnect(sockfd, serverAddr);
 
   struct sockaddr_in clientAddr = createClientAddr(sockfd);
-
-  char ipstr[INET_ADDRSTRLEN] = {'\0'};
-  inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
-  cout << "Set up a connection from: " << ipstr << ":" <<
-    ntohs(clientAddr.sin_port) << endl;
-
-
-  // send/receive data to/from connection
-  bool isEnd = false;
-  string input;
-  char buf[20] = {0};
-  stringstream ss;
-
-  while (!isEnd) {
-    memset(buf, '\0', sizeof(buf));
-
-    cout << "send: ";
-    cin >> input;
-    if (send(sockfd, input.c_str(), input.size(), 0) == -1) {
-      perror("send");
-      return 4;
-    }
-
-
-    if (recv(sockfd, buf, 20, 0) == -1) {
-      perror("recv");
-      return 5;
-    }
-    ss << buf << std::endl;
-    cout << "echo: ";
-    cout << buf << std::endl;
-
-    if (ss.str() == "close\n")
-      break;
-
-    ss.str("");
-  }
+  
+  connectionSetup(clientAddr);
+    
+  communicate(sockfd);
 
   close(sockfd);
 
